ff8dd04d5b2adf3f429c7f8553e74de61af656b7
cell <cellinlab@gmail.com>
2023-03-13 22:59:02 +0800

    init lrwt

---
+++ Learn-React-with-TypeScript/ch01/Alert.jsx
@@ -0,0 +1,11 @@
+export function Alert() {
+  return (
+    <div>
+      <div>
+        <span role="img" aria-label="Warning">⚠</span>
+        <span>Oh no!</span>
+      </div>
+      <div>Something is not quite right ...</div>
+    </div>
+  );
+}
