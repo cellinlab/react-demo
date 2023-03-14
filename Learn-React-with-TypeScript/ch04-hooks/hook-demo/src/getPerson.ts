d8bda59fb54fae12fce65c26c9be9c8d439eaa3a
cell <cellinlab@gmail.com>
2023-03-14 16:32:48 +0800

    code lrwt ch04 effect hooks

---
+++ Learn-React-with-TypeScript/ch04-hooks/hook-demo/src/getPerson.ts
@@ -0,0 +1,13 @@
+type Person = {
+  name: string;
+};
+
+export function getPerson(): Promise<Person> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: 'Cell',
+      });
+    }, 1000);
+  });
+}
