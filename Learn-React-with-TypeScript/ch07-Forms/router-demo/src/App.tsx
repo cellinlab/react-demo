a62f5ab5093886c36f3fc476c0dd5770f54348b1
cell <cellinlab@gmail.com>
2023-03-15 15:12:56 +0800

    code lrwt ch07 refactor uncontrolled fields

--- Learn-React-with-TypeScript/ch07-Forms/router-demo/src/App.tsx
+++ Learn-React-with-TypeScript/ch07-Forms/router-demo/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ContactPage from './ContactPage';
+import ContactPage from './ContactPage2';
 
 function App() {
   return <ContactPage />;
---
+++ Learn-React-with-TypeScript/ch07-Forms/router-demo/src/ContactPage2.tsx
@@ -0,0 +1,49 @@
+import { FormEvent } from 'react';
+
+type Contact = {
+  name: string;
+  email: string;
+  reason: string;
+  notes: string;
+};
+
+export default function ContactPage() {
+  const fieldStyle = 'flex flex-col mb-2';
+  function handleSumit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+  return (
+    <div className="flex flex-col py-10 max-w-md mx-auto">
+      <h2 className="text-3xl font-bold underline mb-3">Contact Us</h2>
+      <p className="mb-3">If you enter you details we will get back to you as soon as possible.</p>
+      <form action="" onSubmit={handleSumit}>
+        <div className={fieldStyle}>
+          <label htmlFor="name">Your name</label>
+          <input type="text" id="name" name="name" />
+        </div>
+        <div className={fieldStyle}>
+          <label htmlFor="email">Your email</label>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div className={fieldStyle}>
+          <label htmlFor="reason">Reason for contact</label>
+          <select id="reason" name="reason">
+            <option value=""></option>
+            <option value="Support">Support</option>
+            <option value="Feedback">Feedback</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+        <div className={fieldStyle}>
+          <label htmlFor="notes">Notes</label>
+          <textarea id="notes" name="notes" cols={30} rows={2}></textarea>
+        </div>
+        <div>
+          <button type="submit" className="mt-2 h-10 px-6 font-semibold bg-black text-white">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
