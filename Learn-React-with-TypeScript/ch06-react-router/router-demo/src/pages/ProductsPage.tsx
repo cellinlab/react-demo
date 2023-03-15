3deceae863c02161b6d715c990ec5c2229199732
cell <cellinlab@gmail.com>
2023-03-15 11:50:30 +0800

    code lrwt ch06 useSearchParams and programmatic navigation using useNavigate

--- Learn-React-with-TypeScript/ch06-react-router/router-demo/src/pages/ProductsPage.tsx
+++ Learn-React-with-TypeScript/ch06-react-router/router-demo/src/pages/ProductsPage.tsx
@@ -1,12 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import { products } from '../data/products';
 
 export function ProductsPage() {
+  const [searchParams] = useSearchParams();
+
+  const getFilteredProducts = () => {
+    const search = searchParams.get('search');
+    if (search === null || search === '') {
+      return products;
+    } else {
+      return products.filter((p) => p.name.toLowerCase().includes(search.toLowerCase()));
+    }
+  };
+
   return (
     <div className="text-center p-5">
       <h2 className="text-xl font-bold text-slate-600">Here are some greet tools for React</h2>
       <ul className="list-none m-0 p-0">
-        {products.map((product) => {
+        {getFilteredProducts().map((product) => {
           return (
             <li key={product.id} className="p-1 text-base text-slate-800">
               <Link
