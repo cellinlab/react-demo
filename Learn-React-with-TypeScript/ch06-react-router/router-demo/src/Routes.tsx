5bedb569730741bcd282e1f93f906076dca07e73
cell <cellinlab@gmail.com>
2023-03-15 10:26:58 +0800

    code lrwt ch06 Link

--- Learn-React-with-TypeScript/ch06-react-router/router-demo/src/Routes.tsx
+++ Learn-React-with-TypeScript/ch06-react-router/router-demo/src/Routes.tsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import { Header } from './Header';
 import { ProductsPage } from './pages/ProductsPage';
 
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Header />,
+  },
   {
     path: 'products',
     element: <ProductsPage />,
