0a36b591ddb637a7340c660bc0de227a990c5b40
cell <cellinlab@gmail.com>
2023-03-15 11:07:36 +0800

    code lrwt ch06 index routes

--- Learn-React-with-TypeScript/ch06-react-router/router-demo/src/Routes.tsx
+++ Learn-React-with-TypeScript/ch06-react-router/router-demo/src/Routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
+import { HomePage } from './pages/HomePage';
 import { ProductsPage } from './pages/ProductsPage';
 import { ProductPage } from './pages/ProductPage';
 import { ErrorPage } from './pages/ErrorPage';
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
       {
         path: 'products',
         element: <ProductsPage />,
