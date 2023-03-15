8f1fa7f8d632b4cb3a65ae7c89cd725b4bbe4996
cell <cellinlab@gmail.com>
2023-03-15 11:02:54 +0800

    code lrwt ch06 errorpage

--- Learn-React-with-TypeScript/ch06-react-router/router-demo/src/Routes.tsx
+++ Learn-React-with-TypeScript/ch06-react-router/router-demo/src/Routes.tsx
@@ -3,11 +3,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import { ProductsPage } from './pages/ProductsPage';
 import { ProductPage } from './pages/ProductPage';
+import { ErrorPage } from './pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: 'products',
