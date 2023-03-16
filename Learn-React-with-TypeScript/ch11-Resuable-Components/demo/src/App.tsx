d363d1707ac3293075b16c1780ed3439037d1e2c
cell <cellinlab@gmail.com>
2023-03-16 17:02:59 +0800

    code lrwt ch11 Resuable Components props spreading

--- Learn-React-with-TypeScript/ch11-Resuable-Components/demo/src/App.tsx
+++ Learn-React-with-TypeScript/ch11-Resuable-Components/demo/src/App.tsx
@@ -7,10 +7,19 @@ function App() {
         data={[
           { id: 1, name: 'Cell', role: 'Manager' },
           { id: 2, name: 'John', role: 'Developer' },
+          { id: 3, name: 'Jane', role: 'Developer' },
+          { id: 4, name: 'Peter', role: 'UI' },
+          { id: 5, name: 'Mary', role: 'UI' },
+          { id: 6, name: 'Mike', role: 'QA' },
         ]}
         id="id"
         primary="name"
         secondary="role"
+        style={{
+          width: '300px',
+          maxHeight: '380px',
+          overflowY: 'auto',
+        }}
       />
     </div>
   );
