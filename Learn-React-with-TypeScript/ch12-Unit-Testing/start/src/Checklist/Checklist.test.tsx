aeeee9930f3cedcd1e05382ee8dfb21bde7b50b7
cell <cellinlab@gmail.com>
2023-03-16 21:30:26 +0800

    code lrwt ch12 Unit Test React Testing simulating user interaction

--- Learn-React-with-TypeScript/ch12-Unit-Testing/start/src/Checklist/Checklist.test.tsx
+++ Learn-React-with-TypeScript/ch12-Unit-Testing/start/src/Checklist/Checklist.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 
 import { Checklist } from "./Checklist"
+import { IdValue } from "../../../../ch11-Resuable-Components/demo/src/CheckList/types"
 
 test("should render correct list items when data sepcified", () => {
   render(
@@ -43,4 +45,40 @@ test("should render correct list items when checkedIds sepcified", () => {
     />
   )
   expect(screen.getByTestId("Checklist__input__1")).toBeChecked()
+})
+
+test("should check items when clicked", async () => {
+  const user = userEvent.setup()
+  render(
+    <Checklist
+      data={[{ id: 1, name: "item 1", role: "Role 1" }]}
+      id="id"
+      primary="name"
+      secondary="role"
+    />
+  )
+  const checkbox = screen.getByTestId("Checklist__input__1")
+  expect(checkbox).not.toBeChecked()
+  await user.click(checkbox)
+  expect(checkbox).toBeChecked()
+})
+
+test("should call onCheckedIdsChange when clicked", async () => {
+  const user = userEvent.setup()
+  let callWith: IdValue[] | undefined = undefined
+  render(
+    <Checklist
+      data={[{ id: 1, name: "item 1", role: "Role 1" }]}
+      id="id"
+      primary="name"
+      secondary="role"
+      onCheckedIdsChange={(checkedIds) => {
+        callWith = checkedIds
+      }}
+    />
+  )
+  const checkbox = screen.getByTestId("Checklist__input__1")
+  expect(checkbox).not.toBeChecked()
+  await user.click(checkbox)
+  expect(callWith).toEqual([1])
 })
