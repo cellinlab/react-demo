020d61ecdc961d936d50f33763cf554d32513894
cell <cellinlab@gmail.com>
2023-03-16 16:45:10 +0800

    code lrwt ch11 Resuable Components generic props

--- Learn-React-with-TypeScript/ch11-Resuable-Components/demo/src/App.tsx
+++ Learn-React-with-TypeScript/ch11-Resuable-Components/demo/src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CheckList } from './CheckList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-10">
+      <CheckList
+        data={[
+          { id: 1, name: 'Cell', role: 'Manager' },
+          { id: 2, name: 'John', role: 'Developer' },
+        ]}
+        id="id"
+        primary="name"
+        secondary="role"
+      />
     </div>
   );
 }
