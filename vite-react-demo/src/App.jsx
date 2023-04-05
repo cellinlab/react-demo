7c3cd336954b3501811c5454d72a929c4456a616
cell <cellinlab@gmail.com>
2023-04-05 20:58:53 +0800

    code init template react with vite

---
+++ vite-react-demo/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-red-500 underline text-center">Hello Vite + React!</h1>
+    </div>
+  );
+}
+
+export default App;
